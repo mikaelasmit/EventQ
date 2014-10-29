c61628c76ecff60a790905d0bbcd2193af37cb08
mikaelasmit <mikaela.smit@gmail.com>
2014-10-29 13:07:21 +0000

    Split EventQ main into functions and main

--- EventQ/main.cpp
+++ EventQ/main.cpp
@@ -2,34 +2,68 @@
 #include <string>
 #include <queue>
 #include <iostream>
+#include <ctime>
 
 using namespace std;  // This is to make available the names of things defined in the standard library.
 
+int HIVInfection(int min, int max)
+{
+	return rand()%(max-min+1)+min;
+}
+
+int Death(int min, int max)
+{
+	return rand()%(max-min+1)+min;
+}
+
+int Period(int min, int max)
+{
+	return rand()%(max-min+1)+min;
+}
+
+
 int main()
 {
+	srand(time(NULL));
+	
 	cout << "Hello, Mikaela!" << endl;
 	
+	// To give each event a date using random numbers between min and max
+	int HIVInfectionNext;
+	HIVInfectionNext=HIVInfection(7,13);
+
+	int DeathNext;
+	DeathNext=Death(1,10);
+
+	int PeriodNext;
+	PeriodNext=Period(2,20);
+
+	// To check what these dates are to proof EventQ
+	cout << "The date of HIV Infection is " << HIVInfectionNext << " years" << endl;
+	cout << "The date of Death is " << DeathNext << " years"<< endl;
+	cout << "The date of the next period is " << PeriodNext << " years" << endl;
+
+	
+	// Event Q definition
 	priority_queue<double, vector<double>, greater<double> > eventQ;
-	
-	double HIVInfection = 10;
-	
-	eventQ.push(HIVInfection);
-	
+
+	// Add events	
+	eventQ.push(HIVInfectionNext);
 	cout << "The next event will happen at " << eventQ.top() << endl;
 	
-	double Death = 20;
-	
-	eventQ.push(Death);
-	
+	eventQ.push(DeathNext);
 	cout << "The next event will happen at " << eventQ.top() << endl;
 	
-	double Period = 5;
-	
-	eventQ.push(Period);
-	
+	eventQ.push(PeriodNext);
 	cout << "The next event will happen at " << eventQ.top() << endl;
 
+	// Remove top events
+	eventQ.pop();
+	cout << "The next event will happen at " << eventQ.top() << endl;
+
+	eventQ.pop();
+	cout << "The next event will happen at " << eventQ.top() << endl;
+	
 	cin.get();
-
 	return 0;
 }
